0cfc0e0af8135596a6e631c0bf84a1e675854c2b
goda emad <goda.emade2001@gmail.com>
2026-08-01 10:47:17 +0300

    Update image_utils.py

--- utils/image_utils.py
+++ utils/image_utils.py
@@ -1,1 +1,18 @@
+import numpy as np
+from PIL import Image, ImageOps
+from core.constants import IMAGE_SIZE
 
+
+def preprocess_image(image: Image.Image) -> np.ndarray:
+    """
+    Resize, normalize and prepare image for model input.
+    - Resize to 224x224
+    - Convert to RGB
+    - Normalize to [-1, 1]
+    - Add batch dimension
+    """
+    image = ImageOps.fit(image, IMAGE_SIZE, Image.Resampling.LANCZOS)
+    image = image.convert("RGB")
+    img_array = np.asarray(image, dtype=np.float32)
+    img_array = (img_array / 127.5) - 1
+    return np.expand_dims(img_array, axis=0)
