c4e6b5ba4d478f3d28d5c0d1566fba3e9eb8a6a8
goda emad <goda.emade2001@gmail.com>
2026-08-01 10:37:02 +0300

    Update predictor.py

--- components/predictor.py
+++ components/predictor.py
@@ -1,1 +1,24 @@
+import numpy as np
+import streamlit as st
+import tensorflow as tf
+from core.constants import MODEL_PATH, CLASS_LABELS
+from utils.image_utils import preprocess_image
 
+
+@st.cache_resource
+def load_model():
+    """Load Keras model once and cache it."""
+    return tf.keras.models.load_model(MODEL_PATH, compile=False)
+
+
+def predict(image):
+    """
+    Run inference on a PIL image.
+    Returns: (preds, top_idx, (emoji, label))
+    """
+    model = load_model()
+    img_array = preprocess_image(image)
+    preds = model.predict(img_array, verbose=0)[0]
+    top_idx = int(np.argmax(preds))
+    emoji, label = CLASS_LABELS[top_idx]
+    return preds, top_idx, (emoji, label)
