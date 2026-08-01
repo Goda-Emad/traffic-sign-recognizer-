ce07628c1d502b90d077e6d2ac1d6ed435ad5ff2
goda emad <goda.emade2001@gmail.com>
2026-08-01 10:39:22 +0300

    Update constants.py

--- core/constants.py
+++ core/constants.py
@@ -1,1 +1,11 @@
+CLASS_LABELS = {
+    0: ("🛑", "Stop"),
+    1: ("⛔", "No Entry"),
+    2: ("➡️", "Keep Right"),
+    3: ("🔵", "Speed Limit 20 km/h"),
+    4: ("🔵", "Speed Limit 30 km/h"),
+}
 
+MODEL_PATH   = "model/keras_model.h5"
+LABELS_PATH  = "model/labels.txt"
+IMAGE_SIZE   = (224, 224)
