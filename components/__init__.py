05d9ae29b2cff84012c313aa04a915fe87286311
goda emad <goda.emade2001@gmail.com>
2026-08-01 10:50:47 +0300

    Update __init__.py

--- components/__init__.py
+++ components/__init__.py
@@ -1,3 +1,3 @@
 from components.predictor import predict, load_model
 from components.ui import render_header, render_result, load_css
-from components.charts import render_confidence_chart
+from components.charts import render_confidence_bar, render_confidence_pie
